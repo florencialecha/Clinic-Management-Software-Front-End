b717bbb8e886ae6f8c9c9d3a2bdd05482b1410a8
lecha_florencia <flecharegueira@gmail.com>
2022-12-05 16:57:04 -0300

    Add notes about Form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 
 import { Outlet } from "react-router-dom";
 
-
-
 function App() {
   return (
       <div className="App">
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,6 +3,16 @@ import React from "react";
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+//Recorda que para la página [/contacto](/docs/funcionalidades.md#pagina-2-contacto) deberás implementar un Form con sus validaciones
+  // Se espera que la página de Contacto, contenga los flujos de validación necesarios (minimo 2 validaciones), para un correcto submit del formulario.
+//   ### Pagina 2: Contacto
+
+// En esta pagina deberán implementar un [Form](/src/Components/Form.jsx) (con sus validaciones pertinentes) que capture la información del usuario que desea contactar con la empresa. Los campos serán los siguientes:
+
+// - Nombre completo (con longitud mayor a 5)
+// - Email (con formato correcto de email)
+// - En caso de haber un error mostrar el siguiente mensaje de error: **Por favor verifique su información nuevamente**
+// - Una vez "enviado"( no se envía a ningún servidor pero podemos mostrar por consola los datos submiteados) el formulario deberán mostrar un mensaje de éxito que contenga el siguiente formato: **Gracias _[nombre del usuario]_, te contactaremos cuando antes vía mail**
 
   return (
     <div>
