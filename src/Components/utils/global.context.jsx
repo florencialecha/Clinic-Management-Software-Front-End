e12d5e713d40f8a8c33ead3b9ecbc432948312cd
lecha_florencia <flecharegueira@gmail.com>
2022-12-07 14:33:35 -0300

    Add coments about globalContext component

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -4,6 +4,13 @@ export const initialState = {theme: "", data: []}
 
 export const ContextGlobal = createContext(undefined);
 
+// ### Paso 3: Implementación del Contexto Global
+
+// Una vez que ya tenemos toda la estructura de nuestra aplicación podemos pasar a consumir la API realizando una llamada por fetch o axios.
+
+// - Deberán guardar dicha información en un contexto global, junto con el theme de la app.
+// - Utilizando useReducer crear los métodos necesarios para el manejo de su comportamiento (cambio de theme de la App y guardado de la respuesta de la API en el Contexto).
+
 const ContextProvider = ({ children }) => {
   //Aqui deberan implementar la logica propia del Context, utilizando el hook useMemo
 
