59fa01089c1ef84e30d18288b97733e8f323cf61
Jonathan Umpiérrez <dev.umpierrez@gmail.com>
2022-12-06 21:49:33 -0300

    useForm hook updated

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,15 +6,35 @@ const Form = () => {
 
   const [submitMessage, setSubmitMessage] = useState();
 
-  const successCallback = () => {
-    setSubmitMessage('Thank you ' + values.fullName + ', we will contact you as soon as possible via email.');
+  const submitCallback = () => {
+    if (validateForm(values)) {
+      setSubmitMessage('Thank you ' + values.fullName + ', we will contact you as soon as possible via email.');
+    } else {
+      setSubmitMessage('Please check your information again.');
+    }
   }
 
-  const errorCallback = () => {
-    setSubmitMessage('Please check your information again.');
+  const validateForm = (values) => {
+    return validateFullName(values.fullName) && validateEmail(values.email);
   }
 
-  const { values, handleChange, handleSubmit } = useForm(successCallback, errorCallback)
+  const validateFullName = (value) => {
+    if (!value || value.length <= 5) {
+      return false;
+    }
+    return true;
+  }
+
+  const validateEmail = (value) => {
+    const emailRegExp = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    if (!value || !emailRegExp.test(value)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  const { values, handleChange, handleSubmit } = useForm(submitCallback);
 
   return (
     <div>
