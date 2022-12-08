2a6b2a7dbecfb0e75733f5498e13034af83c9864
Jonathan Umpiérrez <dev.umpierrez@gmail.com>
2022-12-08 17:57:40 -0300

    List favorites in Favs page

--- src/Pages/Favs.jsx
+++ src/Pages/Favs.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { getFavs } from "../Components/utils/Storage";
 
+import Card from "../Components/Card";
 
 export const Favs = () => {
 
-  /*const [ favs, setFavs ] = useState([]);
+  const [ favs, setFavs ] = useState([]);
 
   useEffect(() => {
-
-  });*/
+    setFavs(getFavs());
+  }, []);
 
   return (
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {
+          favs.map((fav) => <Card name={fav.name} username={fav.username} id={fav.id} kay={fav.id} />)
+        }
       </div>
     </>
   );
