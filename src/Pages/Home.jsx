0e8e5eaa3f65ea61c90c376491bf3cf715754325
Jonathan Umpiérrez <dev.umpierrez@gmail.com>
2022-12-07 23:17:59 -0300

    GlobalContext implemented. Card list in Home page.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
 import Card from '../Components/Card'
+import { ContextGlobal } from '../Components/utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 export const Home = () => {
+
+  const { data, setData } = useContext(ContextGlobal);
+
+  console.log("HOME");
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(res => setData(res.data));
+  }, []);
+
+  console.log(data);
+  
   return (
-    <main className="" >
+    <main className="">
       <h1>Home</h1>
       <div className='card-grid'>
-        {/* Aqui deberias renderizar las cards */}
-        <Card/>
+        {
+          data.map((item) => {
+            return <Card name={item.name} username={item.username} id={item.id} key={item.id} />
+          })
+        }
       </div>
     </main>
   )
