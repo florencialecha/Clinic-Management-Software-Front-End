a7865595485a4bc989a5c0d168251074f6a49823
lecha_florencia <flecharegueira@gmail.com>
2022-12-08 23:50:05 -0300

    Finish basic navbar.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,48 +1,52 @@
 import React from 'react'
-import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { navBar } from '../Routes/NavBarItems'
 
+import AppBar from'@mui/material/AppBar'
 import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FormatAlignLeftIcon from '@mui/icons-material/FormatAlignLeft';
-import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
-import FormatAlignRightIcon from '@mui/icons-material/FormatAlignRight';
-import FormatAlignJustifyIcon from '@mui/icons-material/FormatAlignJustify';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-
-
-import RestoreIcon from '@mui/icons-material/Restore';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import ListItem from '@mui/material/ListItem';
+import Toolbar from '@mui/material/Toolbar';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 /* TODO: Estilos para el logo DH Odonto */
 /* TODO:Deberan implementar ademas la logica para cambiar de Theme con el button */
 
 const Navbar = () => {
-  
 
   return (
-    <nav>
-      <Box>
-          <Link label={'Index'}>DH Odonto</Link>
-          <ul>
-            <li>
-              {navBar.map((item) => (
-                <BottomNavigationAction 
-                  key={item.id} 
-                  label={item.label} 
-                  href={item.path} 
-                  icon={item.Icon} 
-                  showLabel={true}
-                />
-              ))}
-            </li>
-          </ul>
-          <button>🌙</button>
-      </Box>
-    </nav>
+    <AppBar position="static">
+      <Toolbar variant="dense">
+        <ListItem>
+          <Box>
+            <Link to={'/Home'}>DH Odonto</Link>
+          </Box>
+          <Box>
+            {navBar.map((item) => (
+              <BottomNavigationAction 
+                key={item.id} 
+                label={item.label} 
+                href={item.path} 
+                icon={item.Icon} 
+                showLabel={true}
+              />
+            ))}
+            <ToggleButtonGroup>
+              <ToggleButton value='dark'>
+                <DarkModeIcon />
+              </ToggleButton>
+              <ToggleButton value='light'>
+                <LightModeIcon />
+              </ToggleButton>
+            </ToggleButtonGroup>           
+          </Box>
+        </ListItem>
+      </Toolbar>
+    </AppBar>
   )
 
 }
