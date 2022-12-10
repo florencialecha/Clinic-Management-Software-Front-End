d571c6f15c7068c252e9ff6af8ae7cf21a0f464f
Jonathan Umpiérrez <dev.umpierrez@gmail.com>
2022-12-10 16:13:45 -0300

    Theme switcher

---
+++ src/Theme/ThemeSwitcher.jsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "@mui/material"
+import { useEffect } from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import { ContextGlobal } from "../Components/utils/global.context";
+import { Theme } from "./Theme"
+
+export const ThemeSwitcher = ({ children }) => {
+
+    const { theme } = useContext(ContextGlobal);
+
+    const [appTheme, setAppTheme] = useState(Theme(theme));
+
+    useEffect(() => {
+        setAppTheme(Theme(theme));
+    }, [theme]);
+
+    return <ThemeProvider theme={appTheme}>
+        {children}
+    </ThemeProvider>
+}
--- src/index.jsx
+++ src/index.jsx
@@ -4,18 +4,13 @@ import './index.css';
 import AppRouter from './AppRouter';
 import ContextProvider from './Components/utils/global.context'
 
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './Theme';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <ContextProvider>
-        <AppRouter/>
-      </ContextProvider>
-    </ThemeProvider>
+    <ContextProvider>
+      <AppRouter/>
+    </ContextProvider>
   </React.StrictMode>
 );
 
