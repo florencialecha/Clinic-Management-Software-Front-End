c44541d35825fa9e082c0121bc8a141a93eb87e4
florencialecha <flecharegueira@gmail.com>
2022-12-14 17:35:49 -0300

    Add prop baseneme /ctd-esp-fe3-final

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -5,7 +5,7 @@ import { App } from './App'
 
 const AppRouter = () => {
     return (
-      <BrowserRouter basename='ctd-esp-fe3-final'>
+      <BrowserRouter basename='/ctd-esp-fe3-final'>
         <Routes>
           <Route element={<App/>}>
             {internalRoutes.map(({ id, path, Element }) => (<Route key={id} path={path} element={<Element/>}/>))}
