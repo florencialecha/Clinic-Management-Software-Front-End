b6f2a31ed3e149da069b000e52fbe7d40aff7268
florencialecha <flecharegueira@gmail.com>
2022-12-19 18:53:33 -0300

    Try to deploy app on github pages

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -5,7 +5,7 @@ import { App } from './App'
 
 const AppRouter = () => {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/ctd-esp-fe3-final">
         <Routes>
           <Route element={<App/>}>
             {internalRoutes.map(({ id, path, Element }) => (<Route key={id} path={path} element={<Element/>}/>))}
